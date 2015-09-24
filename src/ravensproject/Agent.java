b4b7140d8180e0e2b1f5a01d360dbb0415c82fc9
Justin Jackson <justinjackson@Justins-MacBook-Pro-2.local>
2015-09-24 01:46:02 -0400

    Added in adjacency between figures and semantic networks within figures

--- src/ravensproject/Agent.java
+++ src/ravensproject/Agent.java
@@ -5,6 +5,8 @@ package ravensproject;
 //import java.io.File;
 //import javax.imageio.ImageIO;
 
+import java.util.*;
+
 /**
  * Your Agent for solving Raven's Progressive Matrices. You MUST modify this
  * file.
@@ -28,6 +30,7 @@ public class Agent {
      * 
      */
     public Agent() {
+
 
 
     }
@@ -57,6 +60,104 @@ public class Agent {
      * @return your Agent's answer to this problem
      */
     public int Solve(RavensProblem problem) {
+
+        int cols = Integer.parseInt(problem.getProblemType().substring(0, 1));
+        int rows = cols;
+
+        System.out.println("PROBLEM "+problem.getName()+":");
+
+        Map<String, RavensFigure> figureMap = problem.getFigures();
+        List<Map<String, SemanticNetwork>> semanticList = new ArrayList<>();
+        for(String fig : figureMap.keySet()) {
+            Map<String, SemanticNetwork> semanticMap = buildSemanticNetwork(figureMap.get(fig));
+            //System.out.println(semanticMap.toString());
+            semanticList.add(semanticMap);
+        }
+
+        Map<String, List<String>> adjacencyMap = determineFigureAdjacency(figureMap);
+        compareAdjacentFigures(figureMap, adjacencyMap, semanticList);
+        //compareSemanticNetworks();
+
+        /*
+        Step 1: loop through and create semantic network for each figure
+        Step 2: compare semantic networks to find relations between figures
+            --> create map of relations
+        Step 3: create map of changes between figures based on metrics
+        Step 4:
+         */
+
         return -1;
     }
+
+    public Map<String, SemanticNetwork> buildSemanticNetwork(RavensFigure figure) {
+        Map<String, SemanticNetwork> semanticMap = new HashMap<>();
+
+        SemanticNetwork semanticNetwork = new SemanticNetwork();
+        Map<String, RavensObject> objectMap = figure.getObjects();
+        for(String obj : objectMap.keySet()) {
+            semanticNetwork.addObject(objectMap.get(obj));
+        }
+
+        semanticMap.put(figure.getName(), semanticNetwork);
+        //System.out.println(semanticNetwork.toString());
+
+        return semanticMap;
+    }
+
+    public Map<String, List<String>> determineFigureAdjacency(Map<String, RavensFigure> figureMap) {
+        Map<String, List<String>> adjacencyMap = new HashMap<>();
+
+        // A list to store figures related to problem and not solution (i.e. A, B, C,...)
+        // Only used as a "look-up" reference by name... doesn't actually store RavensFigures
+        List<String> figureList = new ArrayList<>();
+        for(String fig : figureMap.keySet()) {
+            if(fig.matches("[A-Z]")) {
+                figureList.add(fig);
+            }
+        }
+
+        Collections.sort(figureList);
+
+        // Form adjacency between figures to add to map
+        // This should work for all 2x2 and 3x3
+        int size = (int) Math.sqrt(figureList.size() + 1);
+        for(int i = 0; i < figureList.size() - size; i++) {
+            List<String> tempList = new ArrayList<>();
+            if(i % size != 0 || i < size - 1) {
+                tempList.add(figureList.get(i + 1));
+            }
+            tempList.add(figureList.get(i + size));
+            adjacencyMap.put(figureList.get(i), tempList);
+        }
+
+        return adjacencyMap;
+    }
+
+    /**
+     * This method is used for determining the relationship between objects in the problem
+     * figures, not analyze the solution figures.
+     *
+     * @param figureMap
+     * @param adjacencyMap
+     * @param semanticList
+     * @return
+     */
+    public boolean compareAdjacentFigures(
+            Map<String, RavensFigure> figureMap,
+            Map<String, List<String>> adjacencyMap,
+            List<Map<String, SemanticNetwork>> semanticList) {
+
+        
+
+        return false;
+    }
+
+
+
 }
+
+/*
+Todo:
+- maybe make a object class so attributes are easier to compare
+- maybe make a figure class so semantic networks of figures are easier to compare
+ */
